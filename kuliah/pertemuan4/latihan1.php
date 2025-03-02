63ee6ffb1825152cbcf9bd97269ec634494288a6
salmanputraismail <salmanputrai1112@gmail.com>
2025-03-02 21:39:59 +0700

    selesai mengerjakan tugas 3 php

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,14 @@
+<?php
+//while 
+//1.nilai'awal/intial value
+//2.kondisi terminasi/kapan looping
+//3.increment/decrement 
+$i = 10;
+while ($i <=  1) {
+    echo "hello word $i x <br>";
+    $i = $i + 2;
+}
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "HELLO WORD $i x <br>";
+}
